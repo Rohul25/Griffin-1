92c4db30fcacd616fa49546852397ef8fe5c8cff
Griffin43 <120648994+Griffin43@users.noreply.github.com>
2022-12-15 17:44:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Griffin
 fi
-cd /auto
+cd /Griffin
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
